98dfc131bad0b8d406cb572109e9f7d98b991872
YCHino <a2106349@gmail.com>
2018-03-23 21:39:13 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+    <Text>Some Text Hello World! This is my first Mobile App!</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
