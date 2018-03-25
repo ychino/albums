061dd7861441786e397df15036bbf89da17dcb25
YCHino <a2106349@gmail.com>
2018-03-25 14:10:31 -0400

    Add styles and onClick event handler

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-    <View>
+    <View style={{ flex: 1 }}>
         <Header headerText={'Welcome Albums'} />
         <AlbumList />
     </View>
